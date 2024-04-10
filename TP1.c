66923d61fe25256b35a9d670f3e5ff46cae2fa36
Juliette3131 <165903982+Juliette3131@users.noreply.github.com>
2024-04-10 13:20:35 +0200

    Update TP1.c

--- TP1.c
+++ TP1.c
@@ -2,6 +2,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct voisin {
+  int indice;
+  struct voisin *voisinSuivant;
+} voisin;
+
+typedef struct sommet {
+  int indice;
+  struct sommet *suivant;
+  voisin *voisinSuivant;
+} sommet;
+
+typedef struct graphe {
+  sommet *premier;
+} graphe; 
+
+graphe* creerGraphe(){
+  graphe* nouveauGraphe = malloc(sizeof(graphe));
+  if (nouveauGraphe == NULL) {
+    return NULL;
+  }
+  nouveauGraphe -> premier = NULL 
+  return nouveauGraphe;
+}
+
+void creerSommet(graphe* g, int id){
+  sommet* nouveauSommet = malloc(sizeof(sommet));
+  if (nouveauSommet==NULL){
+    return NULL;
+  }
+  nouveauSommet-> indice = id;
+  nouveauSommet -> suivant = g->premier;
+  g->premier = nouveauSommet;
+  nouveauSommet-> voisinSuivant = NULL;
+}
+
 
 int main(){
   
